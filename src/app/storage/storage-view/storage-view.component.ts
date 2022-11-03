1aceb54e1a0e283809a02435acf1558818dddc73
Fabian <sirca.fabian@gmail.com>
2022-11-03 17:37:50 +0100

    update 1.7

---
+++ src/app/storage/storage-view/storage-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'an-storage-view',
+  templateUrl: './storage-view.component.html',
+  styleUrls: ['./storage-view.component.css']
+})
+export class StorageViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
