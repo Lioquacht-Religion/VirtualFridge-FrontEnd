9af5c0ef89ff6c1080a08f771f4fa6f40af7460c
Lioquacht-Religion <basik0010@gmail.com>
2022-11-06 00:50:34 +0100

    refresh page fix

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -18,15 +18,27 @@ export class LoginComponent implements OnInit {
 
 
   login(): void {
-    localStorage.setItem('login_token', 'true');
+    
 
       this.vfservice.getUserData(this.loginemail).subscribe(
         data => { this.user = data; 
           this.vfservice.user = data; 
-          localStorage.setItem('user', this.user.email)
+          //localStorage.setItem('user', this.user.email);
         },
         err => console.log(err),
-        () => console.log('loading done.'+this.user)
+        () => {
+          console.log('loading done.'+this.user);
+          if(this.loginemail === this.user.email){ 
+            localStorage.setItem('user', this.user.email);
+            localStorage.setItem('login_token', 'true');
+            this.vfservice.userLogined = true;
+          }
+          else{
+            this.vfservice.userLogined = false;
+            localStorage.setItem('user', '');
+            localStorage.setItem('login_token', 'false');
+          }
+      }
     );
   }
 
