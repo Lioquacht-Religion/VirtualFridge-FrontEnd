244ebfb75dc1780b9e153d3eb70d69b0e0bfc733
Fabian <sirca.fabian@gmail.com>
2022-11-06 15:20:53 +0100

    tabelle hinzugefügt

---
+++ src/app/recipe/recipe-suggestion/recipe-suggestion.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecipeSuggestionComponent } from './recipe-suggestion.component';
+
+describe('RecipeSuggestionComponent', () => {
+  let component: RecipeSuggestionComponent;
+  let fixture: ComponentFixture<RecipeSuggestionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RecipeSuggestionComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RecipeSuggestionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/recipe/recipe-suggestion/recipe-suggestion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'an-recipe-suggestion',
+  templateUrl: './recipe-suggestion.component.html',
+  styleUrls: ['./recipe-suggestion.component.css']
+})
+export class RecipeSuggestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
