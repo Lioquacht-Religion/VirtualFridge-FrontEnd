66b2c2606833f706c53f577741cd78eba06e12a0
Lioquacht-Religion <basik0010@gmail.com>
2022-11-07 21:43:17 +0100

    acc edit stuff

--- src/app/acc-edit/acc-edit.component.ts
+++ src/app/acc-edit/acc-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { VFridgeService } from '../vfridge-service';
 
 @Component({
   selector: 'an-acc-edit',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./acc-edit.component.css']
 })
 export class AccEditComponent implements OnInit {
+  public userupdname : String = "";
 
-  constructor() { }
+  constructor(public vfservice: VFridgeService) { }
 
   ngOnInit(): void {
+  }
+
+  updateDataOnDB(){
+    let UserToUpdate = {
+      name: this.userupdname,
+      email: this.vfservice.user.email,
+      password: this.vfservice.user.password,
+      id: this.vfservice.user.id
+  };
+    this.vfservice.putUserData(UserToUpdate).subscribe(data => {
+      console.log(data);
+    },
+    err => console.log(err),
+    () => {    
+      this.vfservice.getUserData(this.vfservice.user.email).subscribe(
+      data => { this.vfservice.user = data; },
+      err => console.log(err),
+      () => {
+        console.log('loading done.');
+    }
+  );
+}
+    
+    );
+
+  }
+
+  deleteDataOnDB(){
+  this.vfservice.deleteUserData();
+  this.vfservice.userLogined = false;
+  localStorage.setItem('user', '');
+  localStorage.setItem('login_token', 'false');
   }
 
 }
