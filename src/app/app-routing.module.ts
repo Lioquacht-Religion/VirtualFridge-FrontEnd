70a74ce429c0d6303fc151ff14235ca444a806f5
Lioquacht-Religion <basik0010@gmail.com>
2024-01-09 16:44:19 +0100

    adding shoppinglist comp to routing and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ import { StorageViewComponent } from './storage/storage-view/storage-view.compon
 import { RecipeSuggestionComponent } from './storage/storage-view/recipe-suggestion/recipe-suggestion.component';
 import { AccEditComponent } from './acc-edit/acc-edit.component';
 import { FoodWarningComponent } from './food-warning/food-warning.component';
-import { AppComponent } from './app.component';
+import { ShoppinglistComponent } from './shoppinglist/shoppinglist.component';
 
 
 const routes: Routes = [
@@ -24,64 +24,15 @@ const routes: Routes = [
   { path: 'link_recipeview/:recipeID/:recipeName', component: RecipeViewComponent },
   {path: 'link_recipesuggestion/:storageID' , component: RecipeSuggestionComponent },
   {path: 'link_food-warning' , component: FoodWarningComponent },
-  {path: 'link_acc-edit' , component : AccEditComponent}/*,
-  {
-    path: 'approot',
-    component: AppComponent,
-    children: [
-      {
-        path: 'link_login',
-        component: LoginComponent,
-        outlet: 'login'
-      }
-    ]
-  }*/
+  {path: 'link_acc-edit' , component : AccEditComponent},
+  {path: 'link_shoppinglist/:userID' , component : ShoppinglistComponent}
 ];
 
-const routes2: Routes =[
-  {
-    path: '',
-    component: AppComponent,
-    children: [
 
-{ path: 'link_register', component: RegisterComponent, outlet: 'login' },
-  { path: 'link_login', component: LoginComponent, outlet: 'login'}
-
-    ]
-  },
-{
-    path: 'link_app',
-    component: AppComponent,
-    outlet: 'app',
-    children: [
-
-{ path: 'link_menu', component: MenuComponent, outlet: 'app' },
-  { path: 'link_storage/:userID', component: StorageComponent, outlet: 'app' },
-  { path: 'link_recipe/:userID', component: RecipeComponent },
-  { path: 'link_storageview/:storageID/:storageName', component: StorageViewComponent },
-  { path: 'link_recipeview/:recipeID/:recipeName', component: RecipeViewComponent },
-  {path: 'link_recipesuggestion/:storageID' , component: RecipeSuggestionComponent },
-  {path: 'link_food-warning' , component: FoodWarningComponent },
-  {path: 'link_acc-edit' , component : AccEditComponent}
-
-    ]
-  },
-{ path: 'link_menu', component: MenuComponent },
-  { path: 'link_storage/:userID', component: StorageComponent, outlet: 'app' },
-  { path: 'link_recipe/:userID', component: RecipeComponent },
-  { path: 'link_storageview/:storageID/:storageName', component: StorageViewComponent },
-  { path: 'link_recipeview/:recipeID/:recipeName', component: RecipeViewComponent },
-  {path: 'link_recipesuggestion/:storageID' , component: RecipeSuggestionComponent },
-  {path: 'link_food-warning' , component: FoodWarningComponent },
-  {path: 'link_acc-edit' , component : AccEditComponent}
-
-
-];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes)
-    //RouterModule.forChild(routes2)
   ],
   exports: [RouterModule]
 })
