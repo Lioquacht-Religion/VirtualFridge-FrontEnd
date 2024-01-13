1f0887563da5d9785ea64862f1045fe04b011978
Florian Lotz <florian-lotz@gmx.de>
2024-01-13 17:45:09 +0100

    shopping list functionalities

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { VFridgeService } from 'app/vfridge-service';
 
 @Component({
   selector: 'an-shoppinglist',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppinglistComponent implements OnInit {
 
-  constructor() { }
+  constructor(private vfservice : VFridgeService) {
+
+   }
 
   ngOnInit(): void {
   }
+  storages : any;
+  curUserID : any;
+  storagename = '';
 
+  getStorageFromDB(){
+    this.vfservice.getStorageData(this.curUserID).subscribe(
+      data => { this.storages = data },
+      err => console.log(err),
+      () => console.log('loading done.'+this.storages)
+  );
+  }
+
+  storeDataOnDB(): void {
+    let shoppinglistToCreate = {
+      name: this.storagename,
+      ownerID: this.curUserID,
+      Owner: this.vfservice.getLogedCurUser()
+    };
+    this.vfservice.addStorageData(shoppinglistToCreate).subscribe(data => {
+      console.log(data); },
+      () => {this.getStorageFromDB();}
+      );
+    }
+  
+
+    deleteDataOnDB(UserID: number, StorID: number): void {
+      this.vfservice.deleteStorage(UserID, StorID).subscribe(
+        data => {console.log(data);},
+        () => {this.getStorageFromDB();}
+        );
+      }
 }
