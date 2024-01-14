58c472b4f72272ef7cda011a91ecaeeba95adcc7
Florian Lotz <florian-lotz@gmx.de>
2024-01-14 19:22:22 +0100

    shopping List view changes

--- src/app/shopping-list/shopping-list-view/shopping-list-view.component.ts
+++ src/app/shopping-list/shopping-list-view/shopping-list-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { VFridgeService } from 'app/vfridge-service';
 
 @Component({
   selector: 'an-shopping-list-view',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListViewComponent implements OnInit {
 
-  constructor() { }
+
+  public items : any;
+
+  itemname = '';
+  itemamount = '';
+  itemunit = '';
+
+
+  constructor(private vfservice : VFridgeService) { }
 
   ngOnInit(): void {
+    this.getDataFromDB();
   }
+
+
+  getDataFromDB(){
+    this.vfservice.getShoppinglistItems().subscribe(
+      data => { this.items = data },
+      err => console.log(err),
+      () => console.log('loading done.:' + this.items)
+  );
+  }
+
+  storeDataOnDB(){
+    let itemToCreate = {
+        name: this.itemname,
+        amount: this.itemamount,
+        unit: this.itemunit
+    };
+    this.vfservice.addShoppinglistItem(itemToCreate).subscribe(
+      data => {
+        console.log(data);
+        this.getDataFromDB();
+      },
+        () => this.getDataFromDB()
+    );
+    }
+
+  
 
 }
